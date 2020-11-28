32fbe049a3d5c7fcd928ec0577e55a39f0428ebb
MyungJa <windymork7@naver.com>
2020-11-28 15:21:45 +0900

    javafor

---
+++ src/main/ForMain.java
@@ -0,0 +1,51 @@
+package main;
+
+import process.ForProcess;
+
+import java.util.Scanner;
+
+public class ForMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int choice;
+        System.out.println("크기를 설정해 주세요");
+        int su = scanner.nextInt();
+        ForProcess forProcess = new ForProcess(su);
+
+
+        do {
+            System.out.println("1.정사각형");
+            System.out.println("2.왼쪽 위치 삼각형");
+            System.out.println("3.오른쪽 위치 삼각형");
+            System.out.println("4.피라미드");
+            System.out.println("5.역피라미드");
+            System.out.println("6.마름모");
+            System.out.println("7.모래시계");
+            System.out.println("0.종료");
+            System.out.println("==========================");
+            choice = scanner.nextInt();
+
+
+            switch (choice){
+                case 1: forProcess.moon1();
+                    break;
+                case 2: forProcess.moon2();
+                    break;
+                case 3: forProcess.moon3();
+                    break;
+                case 4: forProcess.moon4();
+                    break;
+                case 5: forProcess.moon5();
+                    break;
+                case 6: forProcess.moon6();
+                    break;
+                case 7: forProcess.moon7();
+                    break;
+            }
+        } while (choice!=0);
+
+    }
+
+}
---
+++ src/process/ForProcess.java
@@ -0,0 +1,114 @@
+package process;
+
+public class ForProcess {
+
+    public int su;
+
+    public ForProcess(int su) {
+        this.su = su;
+    }
+
+    public void moon1() {
+        for (int i = 0; i < su; i++) {
+            for (int j = 0; j < su; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void moon2() {
+        for (int i = 0; i < su; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void moon3(){
+
+        for (int i = 0; i < su; i++) {
+            int cnt = su -i - 1;
+            for (int j = 0; j < cnt; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public void moon4(){
+        for (int i = 0; i < su; i++) {
+            int cnt = su -i -1;
+            for (int j = 0; j < cnt; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= (i*2); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void moon5(){
+        for (int i = 0; i < su; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (su*2)-(i*2)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void moon6(){
+        for (int i = 0; i < su; i++) {
+            int cnt = su -i -1;
+            for (int j = 0; j < cnt; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= (i*2); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < su-1; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < ((su-1)*2)-(i*2)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void moon7(){
+        for (int i = 0; i < su; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (su*2)-(i*2)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < su-1; i++) {
+            int cnt = su -i -1;
+            for (int j = 0; j < cnt-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= ((i+1)*2); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
